e822daae8e01f7752fe54696a3200bf791f5d4c5
Jon Schneider <jon.schneider@gmail.com>
2019-12-27 14:58:30 -0500

    Day 1

---
+++ Day01/Day01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace advent_of_code_2015
+{
+    public class Day01
+    {
+        public void Run()
+        {
+            string input = File.ReadAllText("Day01/input.txt");
+
+            int level = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    level++;
+                }
+                else
+                {
+                    level--;
+                }
+
+                if (level < 0)
+                {
+                    System.Console.WriteLine("Basement was reached at move " + (i + 1));
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace advent_of_code_2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day01 program = new Day01();
+            program.Run();
+        }
+    }
+}
