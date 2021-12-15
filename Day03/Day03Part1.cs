f5e1f25864753a187921656f91f27ca281ada2c8
Jon Schneider <jon.schneider@appfolio.com>
2021-12-15 16:54:50 -0500

    Day 3

---
+++ Day03/Day03Part1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace advent_of_code_2015
+{
+    public class Day03Part1
+    {
+        public void Run()
+        {
+            string inputLine = File.ReadAllLines("Day03/input.txt")[0];
+
+            Point startingLocation = new Point(0, 0);
+            HashSet<Point> visitedPoints = new HashSet<Point>();
+            visitedPoints.Add(startingLocation);
+
+            int x = 0;
+            int y = 0;
+            foreach (char move in inputLine)
+            {
+                switch (move)
+                {
+                    case '>':
+                        x += 1;
+                        break;
+                    case '<':
+                        x -= 1;
+                        break;
+                    case '^':
+                        y += 1;
+                        break;
+                    case 'v':
+                        y -= 1;
+                        break;
+                }
+                visitedPoints.Add(new Point(x, y));
+            }
+
+            Console.WriteLine(visitedPoints.Count);
+        }
+    }
+}
---
+++ Day03/Day03Part2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace advent_of_code_2015
+{
+    public class Day03Part2
+    {
+        public void PerformMove(char move, ref Point position)
+        {
+            switch (move)
+            {
+                case '>':
+                    position.X += 1;
+                    break;
+                case '<':
+                    position.X -= 1;
+                    break;
+                case '^':
+                    position.Y += 1;
+                    break;
+                case 'v':
+                    position.Y -= 1;
+                    break;
+            }
+        }
+
+        public void Run()
+        {
+            string inputLine = File.ReadAllLines("Day03/input.txt")[0];
+
+            Point startingLocation = new Point(0, 0);
+            HashSet<Point> visitedPoints = new HashSet<Point>();
+            visitedPoints.Add(startingLocation);
+
+            Point santaPosition = startingLocation;
+            Point roboSantaPosition = startingLocation;
+            bool santasTurn = true;
+            foreach (char move in inputLine)
+            {
+                if (santasTurn)
+                {
+                    PerformMove(move, ref santaPosition);
+                    visitedPoints.Add(santaPosition); 
+                }
+                else
+                {
+                    PerformMove(move, ref roboSantaPosition);
+                    visitedPoints.Add(roboSantaPosition);
+                }
+                santasTurn = !santasTurn;
+            }
+
+            Console.WriteLine(visitedPoints.Count);
+        }
+    }
+}
