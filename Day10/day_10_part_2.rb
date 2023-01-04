27ac45486dbb7a35049729534c11983efd59c282
Jon Schneider <jon.schneider@appfolio.com>
2023-01-04 07:01:32 -0500

    Day 10

---
+++ Day10/day_10_part_2.rb
@@ -0,0 +1,12 @@
+def look_and_say(input)
+  input.chars.slice_when(&:!=).map { |chunk| "#{chunk.length}#{chunk.first}" }.join
+end
+
+puzzle_input = '1113222113'
+
+result = puzzle_input
+50.times do
+  result = look_and_say(result)
+end
+
+p result.length
