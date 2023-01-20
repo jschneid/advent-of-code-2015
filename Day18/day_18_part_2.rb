79cf7d9d8bcdce280cb2dc9d435d31006e78c86b
Jon Schneider <jon.schneider@appfolio.com>
2023-01-20 08:28:06 -0500

    Day 18

---
+++ Day18/day_18_part_2.rb
@@ -0,0 +1,45 @@
+def count_neighbors_on(grid, x, y)
+  total = 0
+  (y - 1..y + 1).each do |ny|
+    (x - 1..x + 1).each do |nx|
+      next if y == ny && x == nx
+      next if ny < 0 || nx < 0 || ny >= grid.count || nx >= grid[0].count
+
+      total += 1 if grid[ny][nx] == '#'
+    end
+  end
+  total
+end
+
+def animate(grid)
+  new_grid = Array.new(100) { Array.new(100) }
+  (0..99).each do |y|
+    (0..99).each do |x|
+      if [0, 99].include?(y) && [0, 99].include?(x)
+        new_grid[y][x] = '#'
+        next
+      end
+
+      neighbors_on = count_neighbors_on(grid, x, y)
+      new_grid[y][x] = if grid[y][x] == '#'
+                         [2, 3].include?(neighbors_on) ? '#' : '.'
+                       else
+                         neighbors_on == 3 ? '#' : '.'
+                       end
+    end
+  end
+  new_grid
+end
+
+grid = File.readlines('input.txt', chomp: true).map(&:chars)
+grid[0][0] = '#'
+grid[-1][0] = '#'
+grid[0][-1] = '#'
+grid[-1][-1] = '#'
+
+100.times do
+  grid = animate(grid)
+end
+
+p grid.join.count('#')
+
